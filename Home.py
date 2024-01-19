6a2a0f28dc10f530ce2d94cf4a6fe630a165d82b
pgoel813 <147204476+pgoel813@users.noreply.github.com>
2024-01-18 22:47:40 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,6 +12,7 @@ st.markdown("# MATH WORLD")
 #MODEL_NAME = "all-MiniLM-L12-v2"
 EMBEDDING_CSV = "embeddings.csv"
 QUESTION_COLUMN_NAME = "Question"
+ANSWER_COLUMN_NAME = "Answer"
 NUM_CHUNKS = 5
 MODEL = "text-embedding-ada-002"
 
@@ -78,15 +79,16 @@ def extract_embedding(text):
 @st.cache_data
 def load_embeddings():
     embeddings = pd.concat(list(map(pd.read_csv, ["OpenAIchunk_{}.csv".format(chunk_number) for chunk_number in range(NUM_CHUNKS)])), ignore_index = True)
-    return np.array(embeddings.iloc[:,2:].values), embeddings[QUESTION_COLUMN_NAME].to_list()
+    return np.array(embeddings.iloc[:,2:].values), embeddings[QUESTION_COLUMN_NAME].to_list(), embeddings[ANSWER_COLUMN_NAME].to_list()
 
 
 #load models
 embeddings, questions_db = load_embeddings()
 
 
-def get_similar_question(query, num_questions, question_embeddings, main_questions):
+def get_similar_question(query, num_questions, question_embeddings, main_questions, main_answers):
     answers_dict = []
+    questions_dict = []
     #embed the query
     query_embedding = extract_embedding(query)
     query_embedding = np.array(query_embedding).astype(np.float32)
@@ -99,14 +101,15 @@ def get_similar_question(query, num_questions, question_embeddings, main_questio
     indexes = top_scores[1][0]
     #get related question
     for question_index in indexes:
-        answers_dict.append(main_questions[question_index])
+        questions_dict.append(main_questions[question_index])
+        answers_dict.append(main_answers[question_index])
 
-    return answers_dict
+    return questions_dict, answers_dict
 
 
-def answers_holders(*args):
+def answers_holders(*args, answers):
 
-    for index, questions in enumerate(args):
+    for index, (questions, answer) in enumerate(zip(args, answers)):
         answers_div = f"""
             <div class="answers">
                 <h5>Similar Question: {index+1}</h5>
@@ -125,19 +128,20 @@ st.title("Math Minds")
 st.image("https://d2zhlgis9acwvp.cloudfront.net/images/uploaded/mathematicians.jpg", caption = "Mathematics")
 
 #set description
-about_div = """
-    <div class="about">
-        <h3>About the App</h3>
+examples_div = """
+    <div class="Examples">
+        <h3>Example questions to help you get started!</h3>
         <ul>
-            <li>This web app is to sharpen your mathematical skills.</li>
-            <li>You can copy paste any mathematical question and get similar question/questions.</li>
-            <li>Can use them to enhance your skills.</li>
+            <li>A box has 5 dozen bottles of apple juice and half a dozen more bottle of orange juice than apple juice. How many bottles are in the box?</li>
+            <li>Tim can read 10 pages of a book in 30 minutes.  How many hours will it take him to read 120 pages?</li>
+            <li>What is 30 more than a quarter of 40?</li>
+            <li>The price of a home is $120 per square foot. The house is 3000 square feet and the shed is 1000 square feet. How much is this property?</li>
+            <li>If 40 out of 100 people like basketball and out of those that like it, 30% play it, how many people would you expect play basketball out of a group of 300 people?</li>
         </ul>
     </div>
 """
-
 #set the container
-st.markdown(about_div, unsafe_allow_html = True)
+st.markdown(examples_div, unsafe_allow_html = True)
 
 #create tabs
 tab1, tab2 = st.tabs(["Questions 🧮", "Dashboard 📊"])
@@ -168,7 +172,7 @@ with tab1:
 
         if num_questions:
             #get the similar questions
-            similar_questions = get_similar_question([question], num_questions, embeddings, questions_db)
+            similar_questions = get_similar_question([question], num_questions, embeddings, questions_db, answers_db)
 
             #set the answers
             answers_holders(*similar_questions)
@@ -180,3 +184,62 @@ with tab1:
             #define feedback state
             feedback_state = False
             button_state = True
+          
+           with st.sidebar:
+                st.title("Feedback")
+                st.write("Your feedback is much appreciated for improvemment of the web application.")
+                st.subheader("How helpful was this AI Generated Response towards helping you solve this probelm?")
+
+                #create columns
+                col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+
+                with col1:
+                    #set feedback buttons
+                    if st.button("0 (Actively Harmful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "0")
+
+                with col2:
+                    if st.button("1 (Very Harmful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "1")
+
+                with col3:
+                    if st.button("2 (Somewhat Harmful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "2")
+                      
+                with col4:
+                    if st.button("3 (Unlikely to help,but unlikely to hurt)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "3")     
+                      
+                with col5:
+                    if st.button("4 (Somewhat Helpful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "4")
+                      
+                with col6:
+                    if st.button("5 (Very Helpful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "5")
+                      
+                with col7:
+                    if st.button("6 (Extremely Helpful)", type = "primary", on_click=disabled, disabled=st.session_state.disabled, use_container_width = True):
+                        feedback_state = update_feedback(datetime.now(), question, "6")
+ 
+                #say thank you if the feedback is given
+                if feedback_state:
+                    st.header("Thank You For Your Feedback!")
+with tab2:
+    st.header("Feedback Analysis")
+    st.subheader("Feedback Data")
+
+    if not os.path.exists(FEEDBACK_CSV):
+        st.error("Take Some Feedbacks from Users to view the Analytics 🚨")
+    else:
+        #read the data
+        feedbacks = pd.read_csv(FEEDBACK_CSV)
+        st.dataframe(feedbacks)
+        #count plot
+        sns.set_theme(style="whitegrid", palette="pastel")
+        chart_fig = plt.figure(figsize=(5,5))
+        sns.countplot(data = feedbacks, x = FEEDBACK_COLUMN_THREE)
+        plt.xlabel("Feedback")
+        plt.ylabel("Count")
+        plt.title("Feedback Distribution")
+        st.pyplot(chart_fig)
